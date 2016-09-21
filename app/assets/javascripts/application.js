0a4e548ed46797859944a2ab41732ffd01f74677
Douglas Chan <Doug@douglass-mbp.ait-sydney.net.au>
2016-09-21 11:34:43 +1000

    Ratey_rate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
+//= require jquery.raty
+//= require ratyrate
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :books
+  ratyrate_rater
 end
